6cbdb54782c7f267c5b3d7bec170efe29f64501c
Zach Merrill <zachary.merrill@capeelizabethschools.org>
2018-05-07 14:33:48 -0400

    stuff

--- Soccer.js
+++ Soccer.js
@@ -9,29 +9,50 @@ function drawField(){
   ctx.fillRect(0,0,canvas.width,canvas.height);
 
   ctx.fillStyle="#FFFFFF"
-  ctx.fillRect(200,75,5,303);
+  ctx.fillRect(canvas.width/4,canvas.height/6,5,canvas.height*(4/6));
 
   ctx.fillStyle="#FFFFFF"
-  ctx.fillRect(637,75,5,303);
+  ctx.fillRect(canvas.width*(3/4) -5,canvas.height/6,5,canvas.height*(4/6));
 
   ctx.fillStyle="#FFFFFF"
-  ctx.fillRect(0,75,200,5);
+  ctx.fillRect(0,canvas.height/6,canvas.width/4 + 1,5);
   ctx.fillStyle="#FFFFFF"
-  ctx.fillRect(0,373,200,5);
+  ctx.fillRect(0,canvas.height*(5)/6 - 5,canvas.width/4 + 1,5);
 
   ctx.fillStyle="#FFFFFF"
-  ctx.fillRect(637,75,200,5);
+  ctx.fillRect(canvas.width*(3/4) -5,canvas.height/6,canvas.width/4 + 5,5);
   ctx.fillStyle="#FFFFFF"
-  ctx.fillRect(637,373,200,5);
+  ctx.fillRect(canvas.width*(3/4) -5,canvas.height*(5)/6 - 5,canvas.width/4 + 5,5);
 
   ctx.fillStyle="#FFFFFF"
-  ctx.fillRect(419,0,5,453);
+  ctx.fillRect(canvas.width/2,0,5,canvas.height);
 
   ctx.strokeStyle="#FFFFFF"
   ctx.lineWidth = 5;
   ctx.beginPath();
-	ctx.arc(421,226,80,0,2*Math.PI);
+	ctx.arc(canvas.width/2 + 2.5,canvas.height/2 + 5,canvas.height/5,0,2*Math.PI);
 	ctx.stroke();
   }
 
+function drawGoalOne(){
+  ctx.fillStyle="#000000"
+  ctx.fillRect(0,canvas.height*(2/5),5,canvas.height*(1/5));
+  }
+
+function drawGoalTwo(){
+  ctx.fillStyle="#000000"
+  ctx.fillRect(canvas.width - 5,canvas.height*(2/5),5,canvas.height*(1/5));
+  }
+
+function drawBall(){
+  ctx.strokeStyle="#FFFFFF"
+  ctx.lineWidth = 5;
+  ctx.beginPath();
+  ctx.arc(canvas.width/2 + 2.5,canvas.height/2 + 5,canvas.height/5,0,2*Math.PI);
+  ctx.stroke();
+  ctx.fillCircle="000000"
+}
 drawField();
+drawGoalOne();
+drawGoalTwo();
+drawBall();
